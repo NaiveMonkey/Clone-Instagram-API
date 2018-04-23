7d4242a6a0166d42c8d36c9cf182a5c889d6d600
Saemiru Kim <ksmr1102@gmail.com>
2018-04-23 16:52:00 +0900

    create comment on image view

--- clone_instagram_api/images/urls.py
+++ clone_instagram_api/images/urls.py
@@ -7,5 +7,15 @@ urlpatterns = [
         regex=r"^$",
         view=views.Feed.as_view(),
         name='feed'
+    ),
+    url(
+        regex=r"(?P<image_id>[0-9]+)/like/",
+        view=views.LikeImage.as_view(),
+        name='like_image'
+    ),
+    url(
+        regex=r"(?P<image_id>[0-9]+)/comment/",
+        view=views.CommentOnImage.as_view(),
+        name='comment_image'
     )
 ]
--- clone_instagram_api/images/views.py
+++ clone_instagram_api/images/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import models, serializers
+from rest_framework import status
 
 # Create your views here.
 class Feed(APIView):
@@ -26,3 +27,54 @@ class Feed(APIView):
         serializer = serializers.ImagesSerializer(sorted_list, many=True)
 
         return Response(serializer.data)
+
+class LikeImage(APIView):
+
+    def post(self, request, image_id, format=None):
+
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            preexisiting_like = models.Like.objects.get(
+                creator=user,
+                image=found_image
+            )
+            preexisiting_like.delete()
+
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except models.Like.DoesNotExist:
+
+            models.Like.objects.create(
+                creator=user,
+                image = found_image
+            )
+
+            return Response(status=status.HTTP_200_OK)
+
+class CommentOnImage(APIView):
+
+    def post(self, request, image_id, format=None):
+
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = serializers.CommentSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            serializer.save(creator=user, image=found_image)
+
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+        else:
+
+            return Response(data=serializer.errors, status= status.HTTP_405_METHOD_NOT_ALLOWED)

527a43915f561de49031334526499a789e7b1758
SaemiruKim <ksmr1102@gmail.com>
2018-04-25 14:01:30 +0900

    create searching images by hashtag

--- clone_instagram_api/users/serializers.py
+++ clone_instagram_api/users/serializers.py
@@ -4,7 +4,7 @@ from clone_instagram_api.images import serializers as images_serializers
 
 class UserProfileSerializer(serializers.ModelSerializer):
 
-    images = images_serializers.UserProfileImageSerializer(many=True)
+    images = images_serializers.CountImageSerializer(many=True)
 
     class Meta:
         model = models.User

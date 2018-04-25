ee841b8d62de70da7d4b3743f07fb888d630e358
SaemiruKim <ksmr1102@gmail.com>
2018-04-25 17:16:17 +0900

    migrate notification models

--- clone_instagram_api/notifications/models.py
+++ clone_instagram_api/notifications/models.py
@@ -18,3 +18,9 @@ class Notification(image_models.TimeStampedModel):
     notification_type = models.CharField(max_length=20, choices=TYPE_CHOICES)
     image = models.ForeignKey(image_models.Image, on_delete=models.CASCADE, null=True, blank=True)
     comment = models.TextField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return 'From: {} - To: {}'.format(self.creator, self.to)

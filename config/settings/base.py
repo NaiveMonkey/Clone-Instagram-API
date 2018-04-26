aa46b5e758974e03a097d13f4f8619e5a3656c47
SaemiruKim <ksmr1102@gmail.com>
2018-04-26 16:47:28 +0900

    install jwt

--- config/settings/base.py
+++ config/settings/base.py
@@ -244,3 +244,14 @@ SOCIALACCOUNT_ADAPTER = 'clone_instagram_api.users.adapters.SocialAccountAdapter
 # Your stuff...
 # ------------------------------------------------------------------------------
 TAGGIT_CASE_INSENSITIVE = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}

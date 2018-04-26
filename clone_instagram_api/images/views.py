d82075237f762093254594b2eebb297cc6c02ec0
SaemiruKim <ksmr1102@gmail.com>
2018-04-26 14:20:10 +0900

    create deleting image

--- clone_instagram_api/images/views.py
+++ clone_instagram_api/images/views.py
@@ -182,6 +182,15 @@ class Search(APIView):
 
 class ImageDetail(APIView):
 
+    def find_own_image(self, image_id, user):
+
+        try:
+            image = models.Image.objects.get(id=image_id, creator=user)
+            return image
+        except models.Image.DoesNotExist:
+            return None
+
+
     def get(self, request, image_id, format=None):
 
         try:
@@ -197,9 +206,9 @@ class ImageDetail(APIView):
 
         user = request.user
 
-        try:
-            image = models.Image.objects.get(id=image_id, creator=user)
-        except models.Image.DoesNotExist:
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
 
         # 3가지 필드 (file, location, caption)이 다 입력되지 않앋오 업데이트가 가능함
@@ -214,3 +223,16 @@ class ImageDetail(APIView):
         else:
 
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, image_id, format=None):
+
+        user = request.user
+
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        image.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
